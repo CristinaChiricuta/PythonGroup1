18c51912c23691c1cc1bb4b1316bcfbf68aeb820
Letizia Avanzini <lavanzini@deloitte.nl>
2024-03-08 09:45:04 +0100

    importing dataset - reorganization of the code

--- Main.py
+++ Main.py
@@ -1,33 +1,33 @@
 #Investing Project
 
-#part 1. importing the dataset
+#Importing packages
 import pandas as pd
-
 import requests
+
+#Identifying the user
+name = input("What is your username? ")
+print("Hi", name, "!")
+
+#Importing the dataset
+#Asking the user which stock they want
 apikey = 'K8ZT1SX0ZU8ZOPPH'
 symbol = input("What is the name of the stock you are interested in? ")
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&outputsize=full&apikey={apikey}'
 r = requests.get(url)
 data = r.json()
-print(data)
+# print(data)
 
-print(data.keys())
+# print(data.keys())
 data = data['Time Series (5min)']
 df = pd.DataFrame(data).T.apply(pd.to_numeric)
-df.info()
-print(df.head())
+# df.info()
+# print(df.head())
 
+#Visualizing the latest 10 price changes of the selected stock
 df.index = pd.DatetimeIndex(df.index)
-print(df.head(10))
-
+# print(df.head(10))
 df.rename(columns = lambda s: s[3:], inplace=True)
 print(df.head(10))
-
-#part 2. buying assets
-
-#1. Identifyng the user
-name = input("What is your username? ")
-print("Hi", name, "!")
 
 #2. Asking the user which token they want to select
 
